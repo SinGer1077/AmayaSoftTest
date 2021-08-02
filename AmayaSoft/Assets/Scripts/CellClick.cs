0b685272d26166c9faf6e76f14566f192419e627
Singer <SinGer1077@github.com>
2021-08-02 19:40:11 +0500

    Add shake to wrong answer

--- AmayaSoft/Assets/Scripts/CellClick.cs
+++ AmayaSoft/Assets/Scripts/CellClick.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class CellClick : MonoBehaviour
 {
     public GameObject gameController;
     public string this_value;
-   
+    public GameObject childImage;
+    void Awake()
+    {
+        gameController = GameObject.Find("GameController");
+    }
     public void Click()
     {
-
+        if (this_value == gameController.GetComponent<LevelStart>().level_target)
+        {
+            Win();
+        }
+        else
+        {
+            Lose();
+        }
+    }
+    public void Win()
+    {
+        
+    }
+    public void Lose()
+    {
+        childImage.transform.DOShakePosition(2.0f, strength: new Vector3(4, 0, 0), vibrato: 5, randomness: 1, snapping: false, fadeOut: true);
     }
 }
